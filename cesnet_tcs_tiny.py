99a302db40ce9a2c9980e5ab37e2b58ac812b9e2
KyomaHooin <richard.bruna@protonmail.com>
2019-07-18 13:51:25 +0200

    Update logic.

--- cesnet_tcs_tiny.py
+++ cesnet_tcs_tiny.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #
-# Cesnet TCS APIclient
+# Cesnet TCS API client
 #
-# Import TERENA chain cert.
+# Import TERENA chain cert:
 #
 # wget https://pki.cesnet.cz/certs/TERENA_SSL_High_Assurance_CA_3.pem
 # openssl x509 -outform der -in TERENA_SSL_High_Assurance_CA_3.pem -out TERENA_SSL_High_Assurance_CA_3.crt
@@ -18,7 +18,7 @@ TCS_STATUS_URL='/api/v2/certificate/status'
 
 LOG='cesnet_tcs_tiny.log'
 
-#--------------------------
+CLIENT_CRT,CLIENT_KEY,CSR = '','',''
 
 parser = argparse.ArgumentParser(description="Cesnet TCS API tiny client.")
 required = parser.add_argument_group('Required arguments:')
@@ -27,50 +27,44 @@ required.add_argument("--client-key", help="Client AUTH key.",required=True)
 required.add_argument("--csr", help="Server CSR request.",required=True)
 args = parser.parse_args()
 
+log = open(LOG,'a',0)
+
 try:
     with open(args.client-crt,'r') as f: CLIENT_CRT = f.read()
-except:
-    print "Failed to read client certificate."
-    sys.exit(1)
+except: log.write("error: Read client certificate.")
 try:
     with open(args.client-key,'r') as f: CLIENT_KEY = f.read()
-except:
-    print "Failed to read client key."
-    sys.exit(1)
+except: log.write("error: Read client key.")
 try:
     with open(args.csr,'r') as f: CSR = f.read()
-except:
-    print "Failed to read CSR request."
-    sys.exit(1)
+except: log.write("error: Read CSR request.")
 
 TCS_REQUEST_HEADER={'Content-type':'application/json'}
 TCS_REQUEST={
     'certificateRequest': CSR,
-    'certificateType': 'ev',
-    'certificateValidity': 2,
+    'certificateType': 'ov',
+    'certificateValidity': 1,
     'notificationMail': 'admin@company.cz',
     'requesterPhone': '+420314159265'
 }
-TCS_REQUEST_ID=''
 
-#--------------------------
+TCS_REQUEST_ID=''
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS)
 context.load_cert_chain(CLIENT_CRT,CLIENT_KEY)
 
-log = open(LOG,'a',0)
-
-try:
-    c = httplib.HTTPSConnection(TCS_SERVER)
-    c.request('POST',TCS_REQUEST_URL,urllib.urlencode(TCS_REQUEST),HEADER)
-    r = c.getresponse()
-    if r.status == 201:
-        data = json.load(r.read())  
-        if data['status'] == 'ok': TCS_REQUEST_ID = data['id']
-        if data['status'] == 'error':
-            log.write("error: " + data['message'])
-except:
-    log.write("error: API reqest.")
+if CLIENT_CRT and CLIENT_KEY and CSR:
+    try:
+        c = httplib.HTTPSConnection(TCS_SERVER)
+        c.request('POST',TCS_REQUEST_URL,urllib.urlencode(TCS_REQUEST),HEADER)
+        r = c.getresponse()
+        if r.status == 201:
+            data = json.load(r.read())  
+            if data['status'] == 'ok': TCS_REQUEST_ID = data['id']
+            if data['status'] == 'error':
+                log.write("error: " + data['message'])
+    except:
+        log.write("error: API reqest.")
 
 if TCS_REQUEST_ID:
     while 1:
