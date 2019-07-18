ae96372cc45044ab4daba191ba60c5cfcd40d2e7
KyomaHooin <richard.bruna@protonmail.com>
2019-07-18 13:42:45 +0200

    Update logic.

--- cesnet_tcs_tiny.py
+++ cesnet_tcs_tiny.py
@@ -10,70 +10,85 @@
 # update-ca-certificates
 #
 
-import httplib,urllib,json,time,ssl,sys
-
-HEADER={'Content-type':'application/json'}
+import argparse,httplib,urllib,json,time,ssl,sys
 
 TCS_SERVER='tcs.cesnet.cz'
-TCS_STATUS_URL='/api/v2/certificate/status/'
 TCS_REQUEST_URL='/api/v2/certificate/request'
+TCS_STATUS_URL='/api/v2/certificate/status'
+
+LOG='cesnet_tcs_tiny.log'
 
 #--------------------------
 
-print sys.argv
+parser = argparse.ArgumentParser(description="Cesnet TCS API tiny client.")
+required = parser.add_argument_group('Required arguments:')
+required.add_argument("--client-crt", help="Client AUTH certificate.",required=True)
+required.add_argument("--client-key", help="Client AUTH key.",required=True)
+required.add_argument("--csr", help="Server CSR request.",required=True)
+args = parser.parse_args()
 
-#required.add_argument("--client-crt", help="Client AUTH certificate.",required=True)
-#required.add_argument("--client-key", help="Client AUTH key.",required=True)
-#required.add_argument("--csr", help="Server CSR request.",required=True)
-#args = parser.parse_args()
+try:
+    with open(args.client-crt,'r') as f: CLIENT_CRT = f.read()
+except:
+    print "Failed to read client certificate."
+    sys.exit(1)
+try:
+    with open(args.client-key,'r') as f: CLIENT_KEY = f.read()
+except:
+    print "Failed to read client key."
+    sys.exit(1)
+try:
+    with open(args.csr,'r') as f: CSR = f.read()
+except:
+    print "Failed to read CSR request."
+    sys.exit(1)
 
-#try:
-#    with open(args.client-crt,'r') as f: CLIENT_CRT = f.read()
-#except:
-#    print "Failed to load client AUTH certificate."
-#    sys.exit(1)
-
+TCS_REQUEST_HEADER={'Content-type':'application/json'}
 TCS_REQUEST={
-    'certificateRequest': '',
+    'certificateRequest': CSR,
     'certificateType': 'ev',
     'certificateValidity': 2,
     'notificationMail': 'admin@company.cz',
     'requesterPhone': '+420314159265'
 }
+TCS_REQUEST_ID=''
 
+#--------------------------
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-#context.load_cert_chain(CLIENT_CRT,CLIENT_KEY)
+context.load_cert_chain(CLIENT_CRT,CLIENT_KEY)
+
+log = open(LOG,'a',0)
 
 try:
-    c = httplib.HTTPSConnection(TCS_SERVER,timeout=10)
+    c = httplib.HTTPSConnection(TCS_SERVER)
     c.request('POST',TCS_REQUEST_URL,urllib.urlencode(TCS_REQUEST),HEADER)
     r = c.getresponse()
-    print r.read()
     if r.status == 201:
         data = json.load(r.read())  
-        if data['status'] == 'ok':
-            TCS_REQUEST_ID = data['id']
+        if data['status'] == 'ok': TCS_REQUEST_ID = data['id']
         if data['status'] == 'error':
-            print "error: ", data['message']
+            log.write("error: " + data['message'])
 except:
-    print "Reqest error."
+    log.write("error: API reqest.")
 
-if True:
+if TCS_REQUEST_ID:
     while 1:
         try:
-            c = httplib.HTTPSConnection(TCS_SERVER,timeout=10)
-            c.request('GET',TCS_STATUS_URL + '',HADER)
+            c = httplib.HTTPSConnection(TCS_SERVER)
+            c.request('GET',TCS_STATUS_URL + '/' + TCS_REQUEST_ID)
             r = c.getresponse()
             if r.status == 200:
                 data = json.load(r.read())  
-                if data['status'] in ('added','accepted'): pass
                 if data['status'] in ('refused','revoked','error'):
-                   print "error: ", data['message']
+                    log.write("error: " + data['message'])
+                    break
                 if data['status'] == 'issued':
-                    update_cert(data['certificate'])
+                    print data['certificate']
                     break
         except:
-                print "Response error."
+            log.write("error: TCS API response.")
         time.sleep(60)
+
+log.close()
 
